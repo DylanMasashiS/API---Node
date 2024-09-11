1872c75cb701800cc036d7d4b29ada8d643dde91
KianSayuri <159494737+KianSayuri@users.noreply.github.com>
2024-09-10 21:13:54 -0300

    10/09

--- src/controllers/Livros.js
+++ src/controllers/Livros.js
@@ -1,10 +1,10 @@
 // const db = require('../database/connection');
- var fs = require('fs-extra');
+var fs = require('fs-extra');
 
 
-const express = require('express'); 
-const router = express.Router(); 
-const db = require('../database/connection'); 
+const express = require('express');
+const router = express.Router();
+const db = require('../database/connection');
 
 function geraUrl(liv_foto_capa) {
     let img = liv_foto_capa ? liv_foto_capa : 'default.jpg';
@@ -87,99 +87,99 @@ module.exports = {
             });
         }
     },
-    
-    async cadastrarLivros(request, response) {
-    try {
-        // parâmetros recebidos no corpo da requisição
-        const { liv_pha_cod, liv_categ_cod, liv_nome, liv_desc, edt_cod } = request.body;
 
-        const img = request.file.filename;
-        // instrução SQL
-        const sql = `INSERT INTO livros
+    async cadastrarLivros(request, response) {
+        try {
+            // parâmetros recebidos no corpo da requisição
+            const { liv_pha_cod, liv_categ_cod, liv_nome, liv_desc, edt_cod } = request.body;
+
+            const img = request.file.filename;
+            // instrução SQL
+            const sql = `INSERT INTO livros
                 (liv_pha_cod, liv_categ_cod, liv_nome, liv_desc, edt_cod, liv_foto_capa) 
                 VALUES (?, ?, ?, ?, ?, ?)`;
-        // definição dos dados a serem inseridos em um array
-        const values = [liv_pha_cod, liv_categ_cod, liv_nome, liv_desc, edt_cod, img];
-        // execução da instrução sql passando os parâmetros
-        const execSql = await db.query(sql, values);
-        // identificação do ID do registro inserido
-        const liv_cod = execSql[0].insertId;
+            // definição dos dados a serem inseridos em um array
+            const values = [liv_pha_cod, liv_categ_cod, liv_nome, liv_desc, edt_cod, img];
+            // execução da instrução sql passando os parâmetros
+            const execSql = await db.query(sql, values);
+            // identificação do ID do registro inserido
+            const liv_cod = execSql[0].insertId;
 
-        const dados = {
-            liv_cod,
-            liv_pha_cod,
-            liv_categ_cod,
-            liv_nome,
-            liv_desc,
-            edt_cod,
-            liv_foto_capa: 'http://localhost:3333/public/uploads/CapaLivros/' + img
+            const dados = {
+                liv_cod,
+                liv_pha_cod,
+                liv_categ_cod,
+                liv_nome,
+                liv_desc,
+                edt_cod,
+                liv_foto_capa: 'http://localhost:3333/public/uploads/CapaLivros/' + img
+            }
+
+            return response.status(200).json({
+                sucesso: true,
+                mensagem: 'Cadastro do livro efetuado com sucesso.',
+                dados
+                //mensSql: execSql
+            });
+        } catch (error) {
+            return response.status(500).json({
+                sucesso: false,
+                mensagem: 'Erro na requisição.',
+                dados: error.message
+            });
         }
-
-        return response.status(200).json({
-            sucesso: true,
-            mensagem: 'Cadastro do livro efetuado com sucesso.',
-            dados
-            //mensSql: execSql
-        });
-    } catch (error) {
-        return response.status(500).json({
-            sucesso: false,
-            mensagem: 'Erro na requisição.',
-            dados: error.message
-        });
-    }
-},
+    },
     async editarLivros(request, response) {
-    try {
-        // parâmetros recebidos pelo corpo da requisição
-        const { liv_pha_cod, liv_categ_cod, liv_nome, liv_desc, edt_cod, liv_foto_capa } = request.body;
-        // parâmetro recebido pela URL via params ex: /usuario/1
-        const { liv_cod } = request.params;
-        // instruções SQL
-        const sql = `UPDATE livros SET liv_pha_cod = ?, liv_categ_cod = ?, liv_nome = ?, 
+        try {
+            // parâmetros recebidos pelo corpo da requisição
+            const { liv_pha_cod, liv_categ_cod, liv_nome, liv_desc, edt_cod, liv_foto_capa } = request.body;
+            // parâmetro recebido pela URL via params ex: /usuario/1
+            const { liv_cod } = request.params;
+            // instruções SQL
+            const sql = `UPDATE livros SET liv_pha_cod = ?, liv_categ_cod = ?, liv_nome = ?, 
                         liv_desc = ?, edt_cod = ?, liv_foto_capa = ?
                         WHERE liv_cod = ?;`;
-        // preparo do array com dados que serão atualizados
-        const values = [liv_pha_cod, liv_categ_cod, liv_nome, liv_desc, edt_cod, liv_foto_capa, liv_cod];
-        // execução e obtenção de confirmação da atualização realizada
-        const atualizaDados = await db.query(sql, values);
+            // preparo do array com dados que serão atualizados
+            const values = [liv_pha_cod, liv_categ_cod, liv_nome, liv_desc, edt_cod, liv_foto_capa, liv_cod];
+            // execução e obtenção de confirmação da atualização realizada
+            const atualizaDados = await db.query(sql, values);
 
-        return response.status(200).json({
-            sucesso: true,
-            mensagem: `Livro ${liv_cod} atualizado com sucesso!`,
-            dados: atualizaDados[0].affectedRows
-            // mensSql: atualizaDados
-        });
-    } catch (error) {
-        return response.status(500).json({
-            sucesso: false,
-            mensagem: 'Erro na requisição.',
-            dados: error.message
-        });
-    }
-},
+            return response.status(200).json({
+                sucesso: true,
+                mensagem: `Livro ${liv_cod} atualizado com sucesso!`,
+                dados: atualizaDados[0].affectedRows
+                // mensSql: atualizaDados
+            });
+        } catch (error) {
+            return response.status(500).json({
+                sucesso: false,
+                mensagem: 'Erro na requisição.',
+                dados: error.message
+            });
+        }
+    },
     async apagarLivros(request, response) {
-    try {
-        // parâmetro passado via url na chamada da api pelo front-end
-        const { liv_cod } = request.params;
-        // comando de exclusão
-        const sql = `DELETE FROM livros WHERE liv_cod = ?`;
-        // array com parâmetros da exclusão
-        const values = [liv_cod];
-        // executa instrução no banco de dados
-        const excluir = await db.query(sql, values);
+        try {
+            // parâmetro passado via url na chamada da api pelo front-end
+            const { liv_cod } = request.params;
+            // comando de exclusão
+            const sql = `DELETE FROM livros WHERE liv_cod = ?`;
+            // array com parâmetros da exclusão
+            const values = [liv_cod];
+            // executa instrução no banco de dados
+            const excluir = await db.query(sql, values);
 
-        return response.status(200).json({
-            sucesso: true,
-            mensagem: `Livro ${liv_cod} excluído com sucesso`,
-            dados: excluir[0].affectedRows
-        });
-    } catch (error) {
-        return response.status(500).json({
-            sucesso: false,
-            mensagem: 'Erro na requisição.',
-            dados: error.message
-        });
+            return response.status(200).json({
+                sucesso: true,
+                mensagem: `Livro ${liv_cod} excluído com sucesso`,
+                dados: excluir[0].affectedRows
+            });
+        } catch (error) {
+            return response.status(500).json({
+                sucesso: false,
+                mensagem: 'Erro na requisição.',
+                dados: error.message
+            });
+        }
     }
-}
 }
