44476c342fd5049530b698362f411899033a3c4d
Dylan Masashi <159494737+DylanMasashi@users.noreply.github.com>
2024-11-07 20:38:20 -0300

    07 - Multer/middleware

--- src/middlewares/upImgLivros.js
+++ src/middlewares/upImgLivros.js
@@ -1,32 +1,32 @@
-const multer = require('multer'); 
+const multer = require('multer');
+const path = require('path');
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb)  {
-            cb(null, './public/uploads/CapaLivros/');
-    }, 
+    destination: function (req, file, cb) {
+        cb(null, './public/uploads/CapaLivros/'); // Verifique se o diretório existe
+    },
     filename: function (req, file, cb) {
-        //let data = new Date().toISOString().replace(/:/g, '-') + '-';
-        //let data = Date.now().toString();
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9); 
-        // identificar extensão
-        const ext = file.mimetype === 'image/jpeg' ? '.jpeg' : file.mimetype.slice(file.mimetype.length - 3);
+        // Gerar um nome de arquivo único usando timestamp e valor aleatório
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        // Extrair a extensão do arquivo
+        const ext = path.extname(file.originalname);
         cb(null, file.fieldname + '-' + uniqueSuffix + ext);
-        //cb(null, data + '_' + file.originalname);
     }
-}); 
- 
+});
+
 const fileFilter = (req, file, cb) => {
+    // Permitir apenas jpg, jpeg, png
     if (file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
         cb(null, true);
     } else {
-        cb(null, false); 
+        cb(null, false); // Rejeita o arquivo
     }
-} 
- 
-module.exports = (multer({
-    storage: storage, 
-        limits: {
-            fieldSize: 1024 * 1024 * 5
-        }, 
-        fileFilter: fileFilter
-}));
+};
+
+module.exports = multer({
+    storage: storage,
+    limits: {
+        fileSize: 1024 * 1024 * 5 // Limite de 5MB para o arquivo
+    },
+    fileFilter: fileFilter
+});
